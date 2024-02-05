f3b6b91d0e34ad0bb89dedb614db54c423376d09
Mohamed SIDIAN <106597802+Mohamedllx@users.noreply.github.com>
2024-02-05 17:40:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,148 +1,63 @@
-const currentDate = new Date();
-const formattedDate =
-    currentDate.toDateString().split(" ").slice(0, 3).join(" ") +
-    " " +
-    currentDate.toTimeString().split(" ")[0].split(":").slice(0, 3).join(":");
-document.getElementById("dateTime").innerHTML = formattedDate;
-const typingElement = document.querySelector(".typing");
-let index = 0;
-let currentText = "";
-let isDeleting = false;
-let currentMenu = "main";
+"use strict";
 
-const menus = {
-    main: `Select a menu:<br><span onclick="handleMenuClick('1')">[1] Who is Mohamed ?</span><br><span onclick="handleMenuClick('2')">[2] Contact me</span><br><span onclick="handleMenuClick('3')">[3] My works(j'aime pas l'école)</span>`,
-    1: `Who is Mohamed?<br><br>Moi même je sais pas. Quand tu vas le découvrir pardon prévient moi ("_") <br><br><span onclick="handleMenuClick('B')">[B] Back</span>`,
-    2: `Contact:<br>- Email: <a href="http://images.memes.com/meme/1260118">jedonnepas@nakiya.com</a><br>- Numéro: <a href="https://i.imgflip.com/z9mze.jpg">+227°°°°°°°°(CLIQUE POUR AFFICHER)</a><br><br><span onclick="handleMenuClick('B')">[B] Back</span>`,
-    3: `Some of my Projects:<br><br>
-- <strong>RIENN</strong><br><br>
-- <strong>RIENNN</strong><br><br>
-- <strong>RIENNNN</strong><br><br>
-- <strong>RIENNNNN</strong><br><br>
-- <strong>RIENNNNNN</strong><br><br>
-- <strong>RIENNNNNNN</strong><br><br>
-- <strong>RIENNNNNNNN</strong><br><br>
-<span onclick="handleMenuClick('B')">[B] Back</span>`
-};
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
 
-function handleMenuClick(menuKey) {
-    if (menuKey in menus && currentMenu !== menuKey) {
-        isDeleting = true;
-        typeDeleteAnimation(() => {
-            currentMenu = menuKey;
-            currentText = menus[menuKey];
-            index = 0;
-            typeDeleteAnimation();
-        });
-    } else if ((menuKey === "B" || menuKey === "b") && currentMenu !== "main") {
-        isDeleting = true;
-        typeDeleteAnimation(() => {
-            currentMenu = "main";
-            currentText = menus.main;
-            index = 0;
-            typeDeleteAnimation();
-        });
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
     }
-}
-function typeDeleteAnimation(callback) {
-    let speed = 7; // default typing speed
-    let deleteSpeed = 3; // default deletion speed
+  }
+});
 
-    if (currentMenu === "1" || currentMenu === "3") {
-        speed = 1; // Makes the typing faster for "Who is glizzy".
-        deleteSpeed = 1; // Makes the deletion faster for "Who is glizzy". Adjust as needed.
-    }
-
-    if (isDeleting && typingElement.innerHTML !== "") {
-        if (currentText.charAt(index - 1) === ">") {
-            const openTagIndex = currentText.lastIndexOf("<", index);
-            const tagName = currentText.substring(
-                openTagIndex + 1,
-                currentText.indexOf(" ", openTagIndex)
-            );
-            const startTagIndex = currentText.lastIndexOf(
-                `</${tagName}>`,
-                index
-            );
-            index = startTagIndex;
-        } else {
-            index--;
-        }
-        currentText = currentText.slice(0, index);
-        typingElement.innerHTML = currentText;
-
-        setTimeout(() => typeDeleteAnimation(callback), deleteSpeed);
-    } else if (isDeleting) {
-        isDeleting = false;
-        if (callback) callback();
-    } else if (!isDeleting && index < currentText.length) {
-        if (currentText.charAt(index) === "<") {
-            if (currentText.substr(index, 4) === "<br>") {
-                const br = document.createElement("br");
-                typingElement.appendChild(br);
-                index += 4;
-            } else {
-                const closingTagIndex = currentText.indexOf(">", index);
-                const tagName = currentText
-                    .substring(index + 1, closingTagIndex)
-                    .split(" ")[0];
-                const endTagIndex =
-                    currentText.indexOf(`</${tagName}>`, index) +
-                    `</${tagName}>`.length;
-                const outerHTML = currentText.substring(index, endTagIndex);
-                const tempDiv = document.createElement("div");
-                tempDiv.innerHTML = outerHTML;
-                const childElement = tempDiv.firstChild;
-
-                if (tagName === "a") {
-                    childElement.target = "_blank";
-                    speed = 1; // Faster typing for <a> tag
-                } else if (tagName === "span") {
-                    childElement.onclick = function () {
-                        const menuKey = childElement
-                            .getAttribute("onclick")
-                            .replace("handleMenuClick('", "")
-                            .replace("')", "");
-                        handleMenuClick(menuKey);
-                    };
-                    speed = 1; // Faster typing for <span> tag
-                }
-
-                typingElement.appendChild(childElement);
-                index = endTagIndex;
-            }
-        } else {
-            typingElement.innerHTML += currentText.charAt(index);
-            index++;
-        }
-
-        setTimeout(typeDeleteAnimation, speed);
-    }
+function handleYesClick() {
+  titleElement.innerHTML = "Yayyy!! :3";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
 }
 
-function handleUserInput(event) {
-    const key = event.key;
-    if (key in menus && currentMenu !== key) {
-        isDeleting = true;
-        typeDeleteAnimation(() => {
-            currentMenu = key;
-            currentText = menus[key];
-            index = 0;
-            typeDeleteAnimation();
-        });
-    } else if ((key === "B" || key === "b") && currentMenu !== "main") {
-        isDeleting = true;
-        typeDeleteAnimation(() => {
-            currentMenu = "main";
-            currentText = menus.main;
-            index = 0;
-            typeDeleteAnimation();
-        });
-    }
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
 }
 
-document.addEventListener("keydown", handleUserInput);
+function generateMessage(noCount) {
+  const messages = [
+    "No",
+    "Are you sure?",
+    "Pookie please",
+    "Don't do this to me :(",
+    "You're breaking my heart",
+    "I'm gonna cry...",
+  ];
 
-// Initialize the typing animation with the main menu on page load
-currentText = menus.main;
-typeDeleteAnimation();
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
